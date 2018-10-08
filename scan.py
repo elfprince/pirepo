323c229fe608780b586a31d2eb49c604b202a9cd
elfprince <elfprince@users.noreply.github.com>
2018-10-08 19:38:11 +0800

    Add files via upload

---
+++ scan.py
@@ -0,0 +1,6 @@
+class Scan:
+    def __init__(self, id, idetifier, image, date_time):
+        self.id = id
+        self.idetifier = idetifier
+        self.image = image
+        self.date_time = date_time
